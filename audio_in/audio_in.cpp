1d147c8722cb36dcbf8d5b0179442bb28202aee8
yesimzaim <yesimzaim@gmail.com>
2025-08-07 14:54:34 +0200

    Updated code from the example with printing

--- audio_in/audio_in.cpp
+++ audio_in/audio_in.cpp
@@ -1,52 +1,70 @@
-#include "daisy_seed.h"
-#include "daisy_core.h"
 #include "daisy_patch_sm.h"
 #include "daisysp.h"
-#include "daisy_seed.h"
 
+/** These are namespaces for the daisy libraries.
+ *  These lines allow us to omit the "daisy::" and "daisysp::" before
+ * referencing modules, and functions within the daisy libraries.
+ */
 using namespace daisy;
-using namespace daisysp;
 using namespace patch_sm;
+using namespace daisysp;
 
 
-DaisySeed	 hw;
+/** Our hardware board class handles the interface to the actual DaisyPatchSM
+ * hardware. */
 DaisyPatchSM patch;
-Oscillator 	 osc;
-
+/** Callback for processing and synthesizing audio
+ *
+ *  The audio buffers are arranged as arrays of samples for each channel.
+ *  For example, to access the left input you would use:
+ *    in[0][n]
+ *  where n is the specific sample.
+ *  There are "size" samples in each array.
+ *
+ *  The default size is very small (just 4 samples per channel). This means the
+ * callback is being called at 16kHz.
+ *
+ *  This size is acceptable for many applications, and provides an extremely low
+ * latency from input to output. However, you can change this size by calling
+ * patch.SetAudioBlockSize(desired_size). When running complex DSP it can be more
+ * efficient to do the processing on larger chunks at a time.
+ *
+ */
 void AudioCallback(AudioHandle::InputBuffer  in,
-		AudioHandle::OutputBuffer out,
-		size_t                    size)
+                   AudioHandle::OutputBuffer out,
+                   size_t                    size)
 {
-	osc.SetFreq(440);
-
-	/** Process each sample of the oscillator and send to the hardware outputs */
-	for(size_t i = 0; i < size; i++)
-	{
-		float sig = osc.Process();
-		OUT_L[i]  = sig;
-		OUT_R[i]  = sig;
-
-	}
+    /** The easiest way to do pass thru is to simply copy the input to the output
+   * In C++ the standard way of doing this is with std::copy. However, those
+   * familliar with C can use memcpy. A simple loop is also a good way to do
+   * this.
+   *
+   * Since you'll most likely want to be doing something between the input,
+   *  and the output, and not just passing it through we'll demonstrate doing
+   *  so with a for loop.
+   */
+	patch.PrintLine("Input1: %f", in[0][0]);
+ 	patch.PrintLine("Input2: %f", in[1][0]);
+ 
+    for(size_t i = 0; i < size; i++)
+    {
+        out[0][i] = in[0][i]; /**< Copy the left input to the left output */
+        out[1][i] = in[1][i]; /**< Copy the right input to the right output */
+    }
 }
 
-int main(void) {
-
-	// Initialize the Daisy Seed Hardware
-	hw.Init();
+int main(void)
+{
+    /** Initialize the hardware */
+    patch.Init();
 
 	// Enable Logging, and set up the USB connection.
-	// hw.StartLog(true);
-
-	patch.Init();
-
-	osc.Init(patch.AudioSampleRate());
-	osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
-	patch.StartAudio(AudioCallback);
-
-	while(1)
-	{
-
-		// hw.PrintLine("Hello World!");
-		// System::Delay(1000);
-	}
+	patch.StartLog();
+ 
+	// And Print Hello World!
+	//  patch.PrintLine("Hello World!");
+ 
+    /** Start Processing the audio */
+    patch.StartAudio(AudioCallback);
+    while(1) {}
 }
